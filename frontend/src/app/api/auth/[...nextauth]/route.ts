9f6bd29a888683c3f6ed5bb7f390c3ea323a5cf7
Pedro Ecsedei <pedro.martins55757@gmail.com>
2024-05-16 13:16:30 -0300

    build: updating next auth to v5

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,3 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import { handlers } from "../../../../../auth"
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
-    })
-  ]
-})
-
-export { handler as GET, handler as POST }
+export const { GET, POST } = handlers
